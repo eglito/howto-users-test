8c603ac3cedfaab13e6800b27d56ebf9748680e9
eglito <aleeglito@gmail.com>
2025-02-03 23:51:35 -0300

    Erro 500 referentes ao GET resolvidos.

--- src/main/java/com/students/HowTo/Adapters/Output/UserEntity.java
+++ src/main/java/com/students/HowTo/Adapters/Output/UserEntity.java
@@ -6,9 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
     
     @Id
