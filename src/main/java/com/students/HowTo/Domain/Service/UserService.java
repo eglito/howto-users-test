70fb4521d2888e4f44b303d00ca93e33e289a683
eglito <aleeglito@gmail.com>
2025-02-08 10:47:20 -0300

    Alteracao da classe UserService
    Metodos createUser e updateuser passaram a contar com a funcao criptografia e armazenamento dos codigos

--- src/main/java/com/students/HowTo/Domain/Service/UserService.java
+++ src/main/java/com/students/HowTo/Domain/Service/UserService.java
@@ -4,16 +4,20 @@ import com.students.HowTo.Adapters.Output.Repository.UserPersistencyAdapter;
 import com.students.HowTo.Adapters.Output.UserEntity;
 import com.students.HowTo.DTO.UserDto;
 import com.students.HowTo.DTO.UserRequestDTO;
+import com.students.HowTo.Domain.Service.PasswordService.PasswordBcrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements UserServiceInterface {
 
     @Autowired
     private UserPersistencyAdapter userPersistencyAdapter;
+    @Autowired
+    private PasswordBcrypt passwordBcrypt;
 
     public List<UserDto> getAllUsers(){
 
@@ -26,16 +30,28 @@ public class UserService implements UserServiceInterface {
     }
 
     @Override
-    public UserDto createUser(UserRequestDTO userRequestDTO) {
-        userPersistencyAdapter.save(new UserEntity(userRequestDTO.getEmail(), userRequestDTO.getPasswordHash()));
+    public UserDto createUser(UserRequestDTO userRequestDTO) throws IllegalAccessException {
+
+        String encryption = passwordBcrypt.passwordCrypt(userRequestDTO.getPasswordHash());
+
+        Optional<UserEntity> optionalUser = userPersistencyAdapter.findByEmail(userRequestDTO.getEmail());
+        if(optionalUser.isEmpty()){
+            userPersistencyAdapter.save(new UserEntity(userRequestDTO.getEmail(), encryption));
+        }
+
         return new UserDto(userRequestDTO.getEmail());
     }
 
 
     public UserDto updateUser(Long id, UserRequestDTO userRequestDTO){
         UserEntity userEntity = userPersistencyAdapter.findById(id).get();
-        userEntity.setEmail(userRequestDTO.getEmail());
-        userEntity.setPasswordHash(userRequestDTO.getPasswordHash());
+
+        if(userEntity.getEmail() != userRequestDTO.getEmail()){
+            userEntity.setEmail(userRequestDTO.getEmail());
+        }
+        if (userEntity.getPasswordHash() != userRequestDTO.getPasswordHash()){
+            userEntity.setPasswordHash(userRequestDTO.getPasswordHash());
+        }
 
         userPersistencyAdapter.save(userEntity);
 
